18a497321b708eab28d57a63adec15be8117e2c3
Chris Cuilla <ccuilla@gmail.com>
2018-08-31 09:26:58 -0600

    fix: add tests for localhost on URL; update regex

--- src/URL.js
+++ src/URL.js
@@ -3,7 +3,7 @@ import { GraphQLError } from 'graphql/error';
 import { Kind } from 'graphql/language';
 
 const URL_REGEX = new RegExp(
-  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/,
+  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})|https?:\/\/localhost/,
 );
 
 export default new GraphQLScalarType({
--- src/__tests__/URL.test.js
+++ src/__tests__/URL.test.js
@@ -21,6 +21,110 @@ describe('URL', () => {
     });
   });
 
+  describe('valid - localhost', () => {
+    test('serialize', () => {
+      expect(URL.serialize('http://localhost')).toBe('http://localhost');
+    });
+
+    test('parseValue', () => {
+      expect(URL.parseValue('http://localhost')).toBe('http://localhost');
+    });
+
+    test('parseLiteral', () => {
+      expect(
+        URL.parseLiteral({ value: 'http://localhost', kind: Kind.STRING }),
+      ).toBe('http://localhost');
+    });
+  });
+
+  describe('valid - localhost with port', () => {
+    test('serialize', () => {
+      expect(URL.serialize('http://localhost:3000')).toBe(
+        'http://localhost:3000',
+      );
+    });
+
+    test('parseValue', () => {
+      expect(URL.parseValue('http://localhost:3000')).toBe(
+        'http://localhost:3000',
+      );
+    });
+
+    test('parseLiteral', () => {
+      expect(
+        URL.parseLiteral({ value: 'http://localhost:3000', kind: Kind.STRING }),
+      ).toBe('http://localhost:3000');
+    });
+  });
+
+  describe('valid - 0.0.0.0', () => {
+    test('serialize', () => {
+      expect(URL.serialize('http://0.0.0.0')).toBe('http://0.0.0.0');
+    });
+
+    test('parseValue', () => {
+      expect(URL.parseValue('http://0.0.0.0')).toBe('http://0.0.0.0');
+    });
+
+    test('parseLiteral', () => {
+      expect(
+        URL.parseLiteral({ value: 'http://0.0.0.0', kind: Kind.STRING }),
+      ).toBe('http://0.0.0.0');
+    });
+  });
+
+  describe('valid - 0.0.0.0 with port', () => {
+    test('serialize', () => {
+      expect(URL.serialize('http://0.0.0.0:3000')).toBe('http://0.0.0.0:3000');
+    });
+
+    test('parseValue', () => {
+      expect(URL.parseValue('http://0.0.0.0:3000')).toBe('http://0.0.0.0:3000');
+    });
+
+    test('parseLiteral', () => {
+      expect(
+        URL.parseLiteral({ value: 'http://0.0.0.0:3000', kind: Kind.STRING }),
+      ).toBe('http://0.0.0.0:3000');
+    });
+  });
+
+  describe('valid - 127.0.0.1', () => {
+    test('serialize', () => {
+      expect(URL.serialize('http://127.0.0.1')).toBe('http://127.0.0.1');
+    });
+
+    test('parseValue', () => {
+      expect(URL.parseValue('http://127.0.0.1')).toBe('http://127.0.0.1');
+    });
+
+    test('parseLiteral', () => {
+      expect(
+        URL.parseLiteral({ value: 'http://127.0.0.1', kind: Kind.STRING }),
+      ).toBe('http://127.0.0.1');
+    });
+  });
+
+  describe('valid - 127.0.0.1 with port', () => {
+    test('serialize', () => {
+      expect(URL.serialize('http://127.0.0.1:3000')).toBe(
+        'http://127.0.0.1:3000',
+      );
+    });
+
+    test('parseValue', () => {
+      expect(URL.parseValue('http://127.0.0.1:3000')).toBe(
+        'http://127.0.0.1:3000',
+      );
+    });
+
+    test('parseLiteral', () => {
+      expect(
+        URL.parseLiteral({ value: 'http://127.0.0.1:3000', kind: Kind.STRING }),
+      ).toBe('http://127.0.0.1:3000');
+    });
+  });
+
   describe('invalid', () => {
     describe('not a URL', () => {
       test('serialize', () => {
