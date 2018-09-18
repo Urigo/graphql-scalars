642a2bc6523d30c182f9f084a610aa32feea08b9
Tomokatsu Iguchi <tomo14916253@gmail.com>
2018-09-18 23:06:10 +0900

    [fix] regular expression of URL scalar type resolve #17

--- src/__tests__/URL.test.js
+++ src/__tests__/URL.test.js
@@ -4,41 +4,124 @@ import { Kind } from 'graphql/language';
 
 import { URL } from '../';
 
+/*
+ * these test cases are picked up from https://mathiasbynens.be/demo/url-regex
+ */
+
+const SHOULD_MATCH = [
+  'http://foo.com/blah_blah',
+  'http://foo.com/blah_blah/',
+  'http://foo.com/blah_blah_(wikipedia)',
+  'http://foo.com/blah_blah_(wikipedia)_(again)',
+  'http://www.example.com/wpstyle/?p=364',
+  'https://www.example.com/foo/?bar=baz&inga=42&quux',
+  'http://✪df.ws/123',
+  'http://userid:password@example.com:8080',
+  'http://userid:password@example.com:8080/',
+  'http://userid@example.com',
+  'http://userid@example.com/',
+  'http://userid@example.com:8080',
+  'http://userid@example.com:8080/',
+  'http://userid:password@example.com',
+  'http://userid:password@example.com/',
+  'http://142.42.1.1/',
+  'http://142.42.1.1:8080/',
+  'http://➡.ws/䨹',
+  'http://⌘.ws',
+  'http://⌘.ws/',
+  'http://foo.com/blah_(wikipedia)#cite-1',
+  'http://foo.com/blah_(wikipedia)_blah#cite-1',
+  'http://foo.com/unicode_(✪)_in_parens',
+  'http://foo.com/(something)?after=parens',
+  'http://☺.damowmow.com/',
+  'http://code.google.com/events/#&product=browser',
+  'http://j.mp',
+  'ftp://foo.bar/baz',
+  'http://foo.bar/?q=Test%20URL-encoded%20stuff',
+  'http://例子.测试',
+  'http://उदाहरण.परीक्षा',
+  'http://1337.net',
+  'http://a.b-c.de',
+  'http://223.255.255.254',
+  'http://localhost:8080',
+];
+
+const SHOULD_NOT_MATCH = [
+  'http://',
+  'http://.',
+  'http://..',
+  'http://../',
+  'http://?',
+  'http://??',
+  'http://??/',
+  'http://#',
+  'http://##',
+  'http://##/',
+  'http://foo.bar?q=Spaces should be encoded',
+  '//',
+  '//a',
+  '///a',
+  '///',
+  'http:///a',
+  'foo.com',
+  'rdar://1234',
+  'h://test',
+  'http:// shouldfail.com',
+  'http://foo.bar/foo(bar)baz quux',
+  'ftps://foo.bar/',
+  'http://-error-.invalid/',
+  // 'http://a.b--c.de/',
+  'http://-a.b.co',
+  'http://a.b-.co',
+  'http://0.0.0.0',
+  'http://10.1.1.0',
+  'http://10.1.1.255',
+  'http://224.1.1.1',
+  'http://1.1.1.1.1',
+  'http://123.123.123',
+  'http://3628126748',
+  'http://.www.foo.bar/',
+  // 'http://www.foo.bar./',
+  'http://.www.foo.bar./',
+  'http://10.1.1.1',
+];
+
+
 describe('URL', () => {
   describe('valid', () => {
-    test('serialize', () => {
-      expect(URL.serialize('https://test.com')).toBe('https://test.com');
-    });
-
-    test('parseValue', () => {
-      expect(URL.parseValue('https://test.com')).toBe('https://test.com');
-    });
-
-    test('parseLiteral', () => {
-      expect(
-        URL.parseLiteral({ value: 'https://test.com', kind: Kind.STRING }),
-      ).toBe('https://test.com');
+    SHOULD_MATCH.forEach((url) => {
+      test(`serialize ${url}`, () => {
+        expect(URL.serialize(url)).toBe(url);
+      });
+      test(`parseValue ${url}`, () => {
+        expect(URL.parseValue(url)).toBe(url);
+      });
+      test(`parseLiteral ${url}`, () => {
+        expect(
+          URL.parseLiteral({ value: url, kind: Kind.STRING }),
+        ).toBe(url);
+      });
     });
   });
 
   describe('invalid', () => {
     describe('not a URL', () => {
-      test('serialize', () => {
-        expect(() => URL.serialize('this is not a URL')).toThrow(
-          /Value is not a valid URL/,
-        );
-      });
-
-      test('parseValue', () => {
-        expect(() => URL.parseValue('this is not a URL')).toThrow(
-          /Value is not a valid URL/,
-        );
-      });
-
-      test('parseLiteral', () => {
-        expect(() =>
-          URL.parseLiteral({ value: 'this is not a URL', kind: Kind.STRING }),
-        ).toThrow(/Value is not a valid URL/);
+      SHOULD_NOT_MATCH.forEach((url) => {
+        test(`serialize ${url}`, () => {
+          expect(() => URL.serialize(url)).toThrow(
+            /Value is not a valid URL/,
+          );
+        });
+        test(`parseValue ${url}`, () => {
+          expect(() => URL.parseValue(url)).toThrow(
+            /Value is not a valid URL/,
+          );
+        });
+        test(`parseLiteral ${url}`, () => {
+          expect(() =>
+            URL.parseLiteral({ value: url, kind: Kind.STRING }),
+          ).toThrow(/Value is not a valid URL/);
+        });
       });
     });
 
