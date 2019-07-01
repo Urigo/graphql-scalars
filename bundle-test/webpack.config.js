367f26983a4e4c550ded6ed95ffbedfa671a209b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-07-01 02:00:01 -0400

    Fix resolve

--- bundle-test/webpack.config.js
+++ bundle-test/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'graphql-scalars': path.join(__dirname, '../dist/index.js'),
+      'graphql-scalars': path.join(__dirname, '../dist/esnext/index.js'),
     },
     modules: ['node_modules', '../node_modules'],
   },
