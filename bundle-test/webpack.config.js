8f3eb63688544c2c73c56f2aaf00e8253b1342b3
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-07-01 01:58:49 -0400

    Add bundle-size check

---
+++ bundle-test/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  output: {
+    filename: 'index.js',
+  },
+  resolve: {
+    alias: {
+      'graphql-scalars': path.join(__dirname, '../dist/index.js'),
+    },
+    modules: ['node_modules', '../node_modules'],
+  },
+};
