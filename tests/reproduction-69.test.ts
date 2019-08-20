82617d208f43cde1abf68e687fb16107d090b222
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-08-20 00:19:36 -0400

    Reproduction of #69

---
+++ tests/reproduction-69.test.ts
@@ -0,0 +1,37 @@
+import { makeExecutableSchema } from 'graphql-tools';
+import { typeDefs as scalarTypeDefs, resolvers as scalarResolvers } from '../src';
+import { mergeTypes } from 'merge-graphql-schemas';
+import { mergeResolvers } from 'merge-graphql-schemas';
+import { GraphQLSchema, graphql } from 'graphql';
+
+const FOO = { bar: 'QUX' };
+const fooQuery = /* GraphQL */`
+    type Query { 
+        foo: JSON 
+    }
+`;
+const fooResolvers = {
+    Query: {
+        foo: () => FOO
+    },
+};
+
+const typeDefs = mergeTypes([fooQuery, ...scalarTypeDefs]);
+const resolvers = mergeResolvers([fooResolvers, scalarResolvers as any]);
+
+describe('Reproduction issue #69', () => {
+    it('should create a valid schema', async () => {
+        const schema = makeExecutableSchema({
+            typeDefs,
+            resolvers,
+        });
+        expect(schema).toBeInstanceOf(GraphQLSchema);
+        const result = await graphql(schema, /* GraphQL */`
+            {
+                foo
+            }
+        `);
+        expect(result.errors).toBeFalsy();
+        expect(result.data.foo).toBe(FOO);
+    });
+});
