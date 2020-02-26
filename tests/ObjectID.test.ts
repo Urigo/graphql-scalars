c1abea3e30474dd467f2ba63dec2b6865fbd4069
Tunde <tunde1990@gmail.com>
2020-02-26 18:19:50 +0200

    feat(mongodb-objectid): mongodb objectid scalar type
    Adds mongodb objectid scalar type

---
+++ tests/ObjectID.test.ts
@@ -0,0 +1,94 @@
+/* global describe, test, expect */
+
+import { Kind } from 'graphql/language';
+
+import ObjectID from '../src/resolvers/ObjectID';
+
+describe('ObjectId', () => {
+  describe('valid', () => {
+    test('serialize', () => {
+      expect(ObjectID.serialize('5e5677d71bdc2ae76344968c')).toBe('5e5677d71bdc2ae76344968c');
+    });
+
+    test('parseValue', () => {
+      expect(ObjectID.parseValue('5e5677d71bdc2ae76344968c')).toBe('5e5677d71bdc2ae76344968c');
+    });
+
+    test('parseLiteral', () => {
+      expect(
+        ObjectID.parseLiteral({ value: '5e5677d71bdc2ae76344968c', kind: Kind.STRING }),
+      ).toBe('5e5677d71bdc2ae76344968c');
+    });
+  });
+
+  describe('invalid', () => {
+    describe('not a mongodb object id', () => {
+      test('serialize', () => {
+        const invalid = '5e5677d71bdc2ae76344968z';
+        expect(() => ObjectID.serialize(invalid)).toThrow(
+          new RegExp(`Value is not a valid mongodb object id of form: ${invalid}`),
+        );
+      });
+
+      test('parseValue', () => {
+        const invalid = '5e5677d71bdc2ae76344968z';
+        expect(() => ObjectID.parseValue(invalid)).toThrow(
+          new RegExp(`Value is not a valid mongodb object id of form: ${invalid}`),
+        );
+      });
+
+      test('parseLiteral', () => {
+        const invalid = '5e5677d71bdc2ae76344968z';
+        expect(() =>
+          ObjectID.parseLiteral({ value: invalid, kind: Kind.STRING }),
+        ).toThrow(new RegExp(`Value is not a valid mongodb object id of form: ${invalid}`));
+      });
+    });
+
+    describe('too short', () => {
+      test('serialize', () => {
+        const invalid = '5e5677d71bdc2ae';
+        expect(() => ObjectID.serialize(invalid)).toThrow(
+          new RegExp(`Value is not a valid mongodb object id of form: ${invalid}`),
+        );
+      });
+
+      test('parseValue', () => {
+        const invalid = '5e5677d71bdc2ae';
+        expect(() => ObjectID.parseValue(invalid)).toThrow(
+          new RegExp(`Value is not a valid mongodb object id of form: ${invalid}`),
+        );
+      });
+
+      test('parseLiteral', () => {
+        const invalid = '5e5677d71bdc2ae';
+        expect(() =>
+          ObjectID.parseLiteral({ value: invalid, kind: Kind.STRING }),
+        ).toThrow(new RegExp(`Value is not a valid mongodb object id of form: ${invalid}`));
+      });
+    });
+
+    describe('too long', () => {
+      test('serialize', () => {
+        const invalid = '5e5677d71bdc2ae76344968c5';
+        expect(() => ObjectID.serialize(invalid)).toThrow(
+          new RegExp(`Value is not a valid mongodb object id of form: ${invalid}`),
+        );
+      });
+
+      test('parseValue', () => {
+        const invalid = '5e5677d71bdc2ae76344968c5';
+        expect(() => ObjectID.parseValue(invalid)).toThrow(
+          new RegExp(`Value is not a valid mongodb object id of form: ${invalid}`),
+        );
+      });
+
+      test('parseLiteral', () => {
+        const invalid = '5e5677d71bdc2ae76344968c5';
+        expect(() =>
+          ObjectID.parseLiteral({ value: invalid, kind: Kind.STRING }),
+        ).toThrow(new RegExp(`Value is not a valid mongodb object id of form: ${invalid}`));
+      });
+    });
+  });
+});
