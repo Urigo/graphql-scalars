e4f56824b906d1f9080cbd041eb12aacee43b6e2
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-03-24 12:59:18 +0300

    1.0.9

--- src/resolvers/BigInt.ts
+++ src/resolvers/BigInt.ts
@@ -2,18 +2,33 @@
 
 import { Kind, GraphQLScalarType } from 'graphql';
 
+declare global {
+  interface BigInt {
+    toJSON(): string;
+  }
+}
+
 export default function (name = 'BigInt') {
-    return new GraphQLScalarType({
-        name,
-        description:
-            'The `BigInt` scalar type represents non-fractional signed whole numeric values.',
-        serialize: val => val.toString(),
-        parseValue: BigInt,
-        parseLiteral(ast) {
-            if (ast.kind === Kind.INT || ast.kind === Kind.FLOAT || ast.kind === Kind.STRING) {
-                return BigInt(ast.value);
-            }
-            return null;
-        }
-    });
+  BigInt.prototype.toJSON =
+    BigInt.prototype.toJSON ||
+    function (this: bigint) {
+      return this.toString();
+    };
+  return new GraphQLScalarType({
+    name,
+    description:
+      'The `BigInt` scalar type represents non-fractional signed whole numeric values.',
+    serialize: BigInt,
+    parseValue: BigInt,
+    parseLiteral(ast) {
+      if (
+        ast.kind === Kind.INT ||
+        ast.kind === Kind.FLOAT ||
+        ast.kind === Kind.STRING
+      ) {
+        return BigInt(ast.value);
+      }
+      return null;
+    },
+  });
 }
