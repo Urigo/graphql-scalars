4d1163a3a5c57de878d94b9d9ce07910e76b4028
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-04-02 18:59:30 +0300

    v1.1.0

--- tests/Common.test.ts
+++ tests/Common.test.ts
@@ -1,37 +1,43 @@
 import { makeExecutableSchema } from 'graphql-tools';
-import { typeDefs as scalarTypeDefs, resolvers as scalarResolvers } from '../src';
+import {
+  typeDefs as scalarTypeDefs,
+  resolvers as scalarResolvers,
+} from '../src';
 import { mergeTypes } from 'merge-graphql-schemas';
 import { mergeResolvers } from 'merge-graphql-schemas';
 import { GraphQLSchema, graphql } from 'graphql';
 
 const FOO = { bar: 'QUX' };
-const fooQuery = /* GraphQL */`
-    type Query { 
-        foo: JSON 
-    }
+const fooQuery = /* GraphQL */ `
+  type Query {
+    foo: JSON
+  }
 `;
 const fooResolvers = {
-    Query: {
-        foo: () => FOO
-    },
+  Query: {
+    foo: () => FOO,
+  },
 };
 
 const typeDefs = mergeTypes([fooQuery, ...scalarTypeDefs]);
 const resolvers = mergeResolvers([fooResolvers, scalarResolvers as any]);
 
 describe('Common', () => {
-    it('should create a valid schema', async () => {
-        const schema = makeExecutableSchema({
-            typeDefs,
-            resolvers,
-        });
-        expect(schema).toBeInstanceOf(GraphQLSchema);
-        const result = await graphql(schema, /* GraphQL */`
-            {
-                foo
-            }
-        `);
-        expect(result.errors).toBeFalsy();
-        expect(result.data.foo).toBe(FOO);
+  it('should create a valid schema', async () => {
+    const schema = makeExecutableSchema({
+      typeDefs,
+      resolvers,
     });
+    expect(schema).toBeInstanceOf(GraphQLSchema);
+    const result = await graphql({
+      schema,
+      source: /* GraphQL */ `
+        {
+          foo
+        }
+      `,
+    });
+    expect(result.errors).toBeFalsy();
+    expect(result.data.foo).toBe(FOO);
+  });
 });
