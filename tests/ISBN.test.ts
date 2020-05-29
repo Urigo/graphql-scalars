d1719294b18e10f4fc2d59fd6e76d27169f97565
ryan hs <mr.ryansilalahi@gmail.com>
2020-05-29 22:56:52 +0700

    fix: ISBN resolver shouldn't require "ISBN" prefix #347 (#350)

--- tests/ISBN.test.ts
+++ tests/ISBN.test.ts
@@ -4,6 +4,7 @@ import ISBN from '../src/resolvers/ISBN';
 
 // Taken from https://github.com/hapijs/joi/blob/master/test/types/string.js
 const serialNumbers = [
+  // 10 chars with ISBN prefix
   `ISBN 1561616161`,
   `ISBN 1577677171`,
   `ISBN 978 787 78 78`,
@@ -13,6 +14,20 @@ const serialNumbers = [
   `ISBN 979076533X`,
   `ISBN: 978-0615-856`,
   `ISBN: 9780615856`,
+
+  // 10 chars without ISBN prefix
+  `1577677171`,
+  `978 787 78 78`,
+  `978 787 7878`,
+  `978-0615-856`,
+  `9790765335`,
+  `979076533X`,
+  `978-0615-856`,
+  `9780615856`,
+  `9780615856`,
+  `978 787 7878`,
+
+  // 13 chars with ISBN prefix
   `ISBN 978 787 78 78788`,
   `ISBN 978-0615-856-73-5`,
   `ISBN 9790765335999`,
@@ -20,7 +35,14 @@ const serialNumbers = [
   `ISBN-13: 978-0615856735`,
   `ISBN-13: 9780765335999`,
   `ISBN: 978-0615-856-73-5`,
-  `ISBN: 9780615856735`
+  `ISBN: 9780615856735`,
+
+  // 13 chars without ISBN prefix
+  `978-0615-856-73-5`,
+  `978-0615856735`,
+  `9780765335999`,
+  `978-0615-856-73-5`,
+  `9780615856735`,
 ];
 
 describe(`ISBN`, () => {
