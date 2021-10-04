0e5aa9e0adeb94e428cf564533982ffe10709e45
Moshe Hay Boker <mosheboker1@gmail.com>
2021-10-04 16:29:04 +0300

    Guild Docs Design for GraphQL Scalars

---
+++ website/routes.ts
@@ -0,0 +1,34 @@
+import {GenerateRoutes, IRoutes} from '@guild-docs/server';
+
+export function getRoutes(): IRoutes {
+  const Routes: IRoutes = {
+    _: {
+      docs: {
+        $name: 'Docs',
+        $routes: ['README', 'quick-start', 'usage', 'scalars'],
+        _: {
+          usage: {
+            $name: 'Recipes',
+            $routes: ['apollo-server', 'mocks', 'regex']
+          },
+          scalars: {
+            $name: 'Available Scalars',
+            $routes: [
+              'big-int',
+              'byte',
+              'monitoring-and-tracing'
+            ]
+          }
+        }
+      }
+    }
+  };
+  GenerateRoutes({
+    Routes,
+    folderPattern: 'docs',
+    basePath: 'docs',
+    basePathLabel: 'Documentation'
+  });
+
+  return Routes;
+}
