d3294046b7233cca3e66df035bd87734dd571308
TuvalSimha <tuval.simha@gmail.com>
2022-07-20 16:13:31 +0300

    Install

---
+++ website/next.config.mjs
@@ -0,0 +1,28 @@
+import nextBundleAnalyzer from '@next/bundle-analyzer';
+import nextra from 'nextra';
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  unstable_staticImage: true,
+  unstable_flexsearch: {
+    codeblocks: true,
+  },
+});
+
+const withBundleAnalyzer = nextBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' });
+
+export default withBundleAnalyzer(
+  withNextra({
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    redirects: () => [
+      {
+        source: '/docs/introduction',
+        destination: '/docs',
+        permanent: true,
+      },
+    ],
+  })
+);
--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,140 +1,37 @@
 import 'remark-admonitions/styles/infima.css';
 import '../../public/style.css';
 
-import { appWithTranslation } from 'next-i18next';
+import { AppSeoProps } from '@guild-docs/client';
+import { FooterExtended, Header, ThemeProvider } from '@theguild/components';
 
-import { extendTheme, theme as chakraTheme } from '@chakra-ui/react';
-import { mode } from '@chakra-ui/theme-tools';
-import {
-  ExtendComponents,
-  handlePushRoute,
-  CombinedThemeProvider,
-  DocsPage,
-  AppSeoProps,
-} from '@guild-docs/client';
-import { Header, Subheader, FooterExtended } from '@theguild/components';
+import 'nextra-theme-docs/style.css';
+import '../../public/style.css';
+import '@algolia/autocomplete-theme-classic';
+import '@theguild/components/dist/static/css/SearchBarV2.css';
 import Script from 'next/script';
 
 import type { AppProps } from 'next/app';
 
-import '@algolia/autocomplete-theme-classic';
-import '@theguild/components/dist/static/css/SearchBarV2.css';
-
-ExtendComponents({
-  HelloWorld() {
-    return <p>Hello World!</p>;
-  },
-});
-
-const styles: typeof chakraTheme['styles'] = {
-  global: (props) => ({
-    body: {
-      bg: mode('white', 'gray.850')(props),
-    },
-  }),
-};
-
-const theme = extendTheme({
-  colors: {
-    gray: {
-      50: '#fafafa',
-      100: '#f5f5f5',
-      200: '#e5e5e5',
-      300: '#d4d4d4',
-      400: '#a3a3a3',
-      500: '#737373',
-      600: '#525252',
-      700: '#404040',
-      800: '#262626',
-      850: '#1b1b1b',
-      900: '#171717',
-    },
-  },
-  fonts: {
-    heading: 'TGCFont, sans-serif',
-    body: 'TGCFont, sans-serif',
-  },
-  config: {
-    initialColorMode: 'light',
-    useSystemColorMode: false,
-  },
-  styles,
-});
-
 const accentColor = '#1CC8EE';
 
-const serializedMdx = process.env.SERIALIZED_MDX_ROUTES;
-const mdxRoutes = { data: serializedMdx && JSON.parse(serializedMdx) };
-
-function AppContent(appProps: AppProps) {
-  const { Component, pageProps, router } = appProps;
-  const isDocs = router.asPath.startsWith('/docs');
-
+export default function App(appProps: AppProps) {
+  const { Component, pageProps } = appProps;
+  // @ts-expect-error
+  const { getLayout } = Component;
+  const childComponent = <Component {...pageProps} />;
   return (
-    <>
+    <ThemeProvider>
       <Script src="https://the-guild.dev/static/crisp.js" />
       <Header
         accentColor={accentColor}
-        activeLink="/open-source"
         themeSwitch
         searchBarProps={{ version: 'v2' }}
       />
-      <Subheader
-        activeLink={router.asPath}
-        product={{
-          title: 'GraphQL Scalars',
-          description: '',
-          image: {
-            src: '/assets/subheader-logo.svg',
-            alt: 'Docs',
-          },
-          onClick: (e) => handlePushRoute('/', e),
-        }}
-        links={[
-          {
-            children: 'Home',
-            title: 'Read about GraphQL Scalars',
-            href: '/',
-            onClick: (e) => handlePushRoute('/', e),
-          },
-          {
-            children: 'Docs & API',
-            title: 'View docs and examples',
-            href: '/docs',
-            onClick: (e) => handlePushRoute('/docs', e),
-          },
-          {
-            children: 'Github',
-            target: '_blank',
-            rel: 'noopener norefereer',
-            title: "Head to the project's GitHub",
-            href: 'https://github.com/urigo/graphql-scalars',
-          },
-        ]}
-        cta={{
-          children: 'Get Started',
-          title: 'Start using GraphQL Scalar',
-          href: '/docs',
-          rel: 'noopener noreferrer',
-        }}
-      />
-      {isDocs ? (
-        <DocsPage
-          appProps={appProps}
-          accentColor={accentColor}
-          mdxRoutes={mdxRoutes}
-        />
-      ) : (
-        <Component {...pageProps} />
-      )}
+      {getLayout ? getLayout(childComponent) : childComponent}
       <FooterExtended />
-    </>
+    </ThemeProvider>
   );
 }
-
-const AppContentWrapper = appWithTranslation(function TranslatedApp(appProps) {
-  return <AppContent {...appProps} />;
-});
 
 const defaultSeo: AppSeoProps = {
   title: 'GraphQL Scalars',
@@ -145,15 +42,3 @@ const defaultSeo: AppSeoProps = {
     height: 54,
   },
 };
-
-export default function App(appProps: AppProps) {
-  return (
-    <CombinedThemeProvider
-      theme={theme}
-      accentColor={accentColor}
-      defaultSeo={defaultSeo}
-    >
-      <AppContentWrapper {...appProps} />
-    </CombinedThemeProvider>
-  );
-}
