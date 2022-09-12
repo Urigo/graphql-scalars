66d53a843a76e93a33ec50942827a83b7a805479
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-09-12 17:48:47 +0300

    ...

--- website/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require('/home/ardat_000/Guild/graphql-scalars/website/node_modules/remark-admonitions/styles/infima.css'),
-  require('/home/ardat_000/Guild/graphql-scalars/website/node_modules/remark-admonitions/styles/infima.css'),
-  require('/home/ardat_000/Guild/graphql-scalars/website/node_modules/remark-admonitions/styles/infima.css'),
-  require('/home/ardat_000/Guild/graphql-scalars/website/node_modules/infima/dist/css/default/default.css'),
-  require('/home/ardat_000/Guild/graphql-scalars/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/ardat_000/Guild/graphql-scalars/website/src/css/custom.css'),
-];
