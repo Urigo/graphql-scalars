ee7fac44419417abcec5dd35a5d5c91cbb0c7759
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-11 00:19:24 +0200

    prettier

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,33 +1,31 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint sort-keys: error */
-import { defineConfig, Giscus, useTheme } from '@theguild/components'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import { defineConfig, Giscus, useTheme } from '@theguild/components';
 
 export default defineConfig({
   docsRepositoryBase: 'https://github.com/urigo/graphql-scalars/tree/master/website', // base URL for the docs repository
-  main({ children }) {
-    const { resolvedTheme } = useTheme()
-    const { route } = useRouter()
+  main: function Main({ children }) {
+    const { resolvedTheme } = useTheme();
+    const { route } = useRouter();
 
     const comments = route !== '/' && (
       <Giscus
-      // ensure giscus is reloaded when client side route is changed
-      key={route}
-      repo="Urigo/graphql-scalars"
-      repoId="MDEwOlJlcG9zaXRvcnk5NDU2MjE3Mw=="
-      category="Docs Discussions"
-      categoryId="DIC_kwDOBaLnfc4CSDVs"
-      mapping="pathname"
-      theme={resolvedTheme}
-    />
-    )
+        // ensure giscus is reloaded when client side route is changed
+        key={route}
+        repo="Urigo/graphql-scalars"
+        repoId="MDEwOlJlcG9zaXRvcnk5NDU2MjE3Mw=="
+        category="Docs Discussions"
+        categoryId="DIC_kwDOBaLnfc4CSDVs"
+        mapping="pathname"
+        theme={resolvedTheme}
+      />
+    );
     return (
       <>
         {children}
         {comments}
       </>
-    )
+    );
   },
   siteName: 'SCALARS',
-})
-
+});
